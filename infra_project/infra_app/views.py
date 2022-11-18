0cee73feb4819f6473ad1ac02fba27ed784683a0
Dmitry Sukharev <dmitri.sukharev@gmail.com>
2022-11-18 22:34:38 +0300

    fake fail to check CI linter check

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -4,6 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('У меня получилось! <br>CI/CD works ok!')
 
-
 def second_page(request):
     return HttpResponse('А это вторая страница!')
