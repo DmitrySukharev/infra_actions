a13527c4703c7df24114fc4c6e85192a8acdfaaa
Dmitry Sukharev <dmitri.sukharev@gmail.com>
2022-11-18 22:42:11 +0300

    fake fail to check CI Django tests

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('У меня получилось! <br>CI/CD works ok!')
 
+
 def second_page(request):
     return HttpResponse('А это вторая страница!')
